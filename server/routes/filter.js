739f74b859295060e6cdfa3b97a4614fc857a324
prateek-kgupta <pkgupta1814@gmail.com>
2023-05-11 16:36:58 +0530

    space

--- server/routes/filter.js
+++ server/routes/filter.js
@@ -2,32 +2,32 @@ const express = require('express')
 const router = express.Router()
 const client = require('../connection/db')
 
-router.get('/alldata',async (req,res)=>{
-       try {
-        
-        await client.query("BEGIN");
-    
-   
-     await client.query(`SELECT *
+router.get('/alldata', async (req, res) => {
+  try {
+
+    await client.query("BEGIN");
+
+
+    await client.query(`SELECT *
      FROM applicant_iteration_master
      LEFT JOIN application_status_audit
-     ON applicant_iteration_master.id = application_status_audit.user_id;`,(error,result)=>{
-        if(error){
-            throw error
-        }
-        else{
-            
-            res.status(200).send(result);
-        }
-     });
-        
-        await client.query("COMMIT");
-       
-      } catch (err) {
-        console.log(err)
-      
-        
-        res.status(500).send("Error");
-      } 
+     ON applicant_iteration_master.id = application_status_audit.user_id;`, (error, result) => {
+      if (error) {
+        throw error
+      }
+      else {
+
+        res.status(200).send(result);
+      }
+    });
+
+    await client.query("COMMIT");
+
+  } catch (err) {
+    console.log(err)
+
+
+    res.status(500).send("Error");
+  }
 })
 module.exports = router
