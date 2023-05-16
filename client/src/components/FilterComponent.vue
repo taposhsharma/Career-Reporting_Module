c6a6dfaade4a8c27af71be9dcd63fb23e7825ff1
enm-2001 <ektamer4@gmail.com>
2023-05-16 13:02:56 +0530

    updated

--- client/src/components/FilterComponent.vue
+++ client/src/components/FilterComponent.vue
@@ -1,6 +1,90 @@
 <template>
-  <div class="filter">
-      <!-- <li><button @click="experience = !experience">Experience</button></li> -->
+<div>
+<div class="filter">
+    <div class="row">
+      <div class="col-4">Position</div>
+      <div class="col-8 dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          @click="toggleDropdownPos"
+          aria-haspopup="true"
+          aria-expanded="isDropdownOpenPos"
+        >
+          Position
+        </button>
+        <div class="dropdown-menu" :class="{ show: isDropdownOpenPos }">
+          <a
+            v-for="item in Array.from(new Set(this.posOptions))"
+            :key="item"
+            class="dropdown-item"
+            @click="toggleItemPos(item)"
+          >
+            {{ item }}
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4">Location</div>
+      <div class="dropdown">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        @click="toggleDropdownLoc"
+        aria-haspopup="true"
+        aria-expanded="isDropdownOpenLoc"
+      >
+        Location: {{selectedLoc.length}}
+      </button>
+      <div class="dropdown-menu" :class="{ show: isDropdownOpenLoc }">
+        <a
+          v-for="item in Array.from(new Set(this.locOptions))"
+          :key="item"
+          class="dropdown-item"
+          @click="toggleItemLoc(item)"
+        >
+          {{ item }}
+        </a>
+      </div>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-4">Interview Results</div>
+    </div>
+    <div class="row">
+      <div class="col-4">Gender</div>
+    </div>
+    <div class="row">
+      <div class="col-4">Experience</div>
+      <div class="col-8 d-flex align-items-center">
+        <input type="number" min="0" max="15" v-model="rangeExperience[0]" />
+        <vue-slider
+          v-model="rangeExperience"
+          :min="0"
+          :max="15"
+          class="w-100 ml-3 mr-3"
+        ></vue-slider>
+        <input type="number" min="0" max="15" v-model="rangeExperience[1]" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4">Age</div>
+      <div class="col-8 d-flex align-items-center">
+        <input type="number" min="18" max="75" v-model="rangeAge[0]" />
+        <vue-slider
+          v-model="rangeAge"
+          :min="18"
+          :max="75"
+          class="w-100 ml-3 mr-3"
+        ></vue-slider>
+        <input type="number" min="0" max="15" v-model="rangeAge[1]" />
+      </div>
+    </div>
+    <button class="filButton">Filter</button>
+      <button class="filButton" @click="closeFilters()">Close</button>
+  </div>
+  <!-- <div class="filter">
       <div class="div">
         <label id="experience">Experience:</label> &nbsp; 
         <div class="experience">     
@@ -10,52 +94,23 @@
         </div>
         <br>
       <vue-slider v-model="rangeExperience" :min="0" :max="100"></vue-slider>
-      <!-- <li><button @click="position = !position">Position</button></li> -->
-      <!-- <div v-if="position" class="div">
-          <label for="position">Position:</label>
-          <select v-model="selectedTech" multiple size="4">
-            <option v-for="option in Array.from(new Set(this.posOptions))" :value="option" :key="option">
-              {{ option }}
-            </option>
-          </select>
-        </div> -->
-        <!-- <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-    Position
-  </button>
-  <div class="dropdown-menu">
-    <select v-model="selectedTech" multiple size="4">
-            <option v-for="option in Array.from(new Set(this.posOptions))" :value="option" :key="option">
-              {{ option }}
-            </option>
-          </select>
-  </div>
-</div> -->
-<!-- <b-dropdown v-model="selectedPos" toggle-class="btn-primary" no-close-on-select>
-      <template v-slot:button-content>
-        <span>Selected items: {{ selectedPos.length }}</span>
-      </template>
-      <b-dropdown-item v-for="item in Array.from(new Set(this.posOptions))" :key="item" @click="toggleItem(item)">
-        {{ item }}
-      </b-dropdown-item>
-    </b-dropdown> -->
     
     <div class="dropdown">
       <button
-        class="btn btn-primary dropdown-toggle"
+        class="btn btn-secondary dropdown-toggle"
         type="button"
         @click="toggleDropdownPos"
         aria-haspopup="true"
         aria-expanded="isDropdownOpenPos"
       >
-        Selected items: {{ selectedPos.length }}
+        Position: {{selectedPos.length}}
       </button>
       <div class="dropdown-menu" :class="{ show: isDropdownOpenPos }">
         <a
           v-for="item in Array.from(new Set(this.posOptions))"
           :key="item"
           class="dropdown-item"
-          @click="toggleItem(item)"
+          @click="toggleItemPos(item)"
         >
           {{ item }}
         </a>
@@ -65,11 +120,7 @@
       <button @click="gender = !gender">Gender</button>
       <div v-if="gender" class="div">
           <label for="gender">Gender:</label>
-          <select id="gender">
-            <option v-for="option in Array.from(new Set(this.genderOptions))" :value="option" :key="option">
-              {{ option }}
-            </option>
-          </select>
+          <input type="checkbox" v-for="option in Array.from(new Set(this.genderOptions))" :value="option" :key="option">{{option}}
         </div>
 
       <div class="div" >
@@ -83,23 +134,31 @@
       <vue-slider v-model="rangeAge" :min="0" :max="100"></vue-slider>
       
 <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button"  @click="toggleDropdownLoc" aria-expanded="isDropdownOpenLoc">
-    Location
-  </button>
-  <div class="dropdown-menu" :class="{ show: isDropdownOpenLoc }">
-          <select v-model="selectedLoc" multiple size="4" class="location">
-            <option v-for="option in Array.from(new Set(this.locOptions))" :value="option" :key="option">
-              {{ option }}
-            </option>
-          </select>
-        </div>
-</div>
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        @click="toggleDropdownLoc"
+        aria-haspopup="true"
+        aria-expanded="isDropdownOpenLoc"
+      >
+        Location: {{selectedLoc.length}}
+      </button>
+      <div class="dropdown-menu" :class="{ show: isDropdownOpenLoc }">
+        <a
+          v-for="item in Array.from(new Set(this.locOptions))"
+          :key="item"
+          class="dropdown-item"
+          @click="toggleItemLoc(item)"
+        >
+          {{ item }}
+        </a>
+      </div>
+    </div>
 <br>
       <button class="filButton">Filter</button>
-      <!-- <button class="filButton" @click="open = false">Close</button> -->
       <button class="filButton" @click="closeFilters()">Close</button>
-  </div>
-
+  </div> -->
+</div>
 </template>
 
 <script>
@@ -126,12 +185,10 @@ export default {
       isDropdownOpenPos: false,
       isDropdownOpenLoc: false,
 
-      experienceOptions: [1,2,3,4,5,6,7,8],
-      ageOptions: ["20-30", "30-40", "40-50", "50-60"],
       genderOptions: [],
       locOptions: [],
       selectedLoc: [],
-      selectedTech: [],
+
     };
   },
   emits: ['close'],
@@ -145,11 +202,18 @@ export default {
     toggleDropdownLoc() {
       this.isDropdownOpenLoc = !this.isDropdownOpenLoc;
     },
-    toggleItem(item) {
+    toggleItemPos(item) {
       if (this.selectedPos.includes(item)) {
         this.selectedPos = this.selectedPos.filter((i) => i !== item);
       } else {
         this.selectedPos.push(item);
+      }
+    },
+    toggleItemLoc(item) {
+      if (this.selectedLoc.includes(item)) {
+        this.selectedLoc = this.selectedLoc.filter((i) => i !== item);
+      } else {
+        this.selectedLoc.push(item);
       }
     },
   },
@@ -170,15 +234,15 @@ export default {
 
 <style scoped>
 .filter {
-  margin: auto;
+  /* margin: auto; */
   width: 100%;
   background-color: #f5f5f5;
   /* width: 200px; */
   box-shadow: 0px 2px 4px #888;
-  /* position: fixed; */
   top: 60px;
   padding: 20px;
-  text-align: center;
+  /* text-align: center; */
+  align-items: center;
   user-select: none;
 }
 
@@ -193,7 +257,9 @@ export default {
 }
 
 .div {
+  margin-left: auto;
   display: flex;
+  align-items: center;
   /* align-content: center; */
   /* width: 200px; */
   margin-bottom: 10px;
@@ -205,12 +271,12 @@ export default {
 input {
   width: 50px;
 }
-
 .filButton{
     background-color: grey;
     cursor: pointer;
 }
 .location{
+  height: 100%;
   width: 100%;
 }
 </style>
